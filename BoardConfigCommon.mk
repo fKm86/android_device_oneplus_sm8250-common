07d00f167a68e8aab75b9ed73fcecafdc44c7f84
fKm <fmetzner86@gmail.com>
2020-08-03 11:23:13 +0200

    sm8250-common: build with proton clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,7 +54,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := latest
+TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
 
 # Platform
