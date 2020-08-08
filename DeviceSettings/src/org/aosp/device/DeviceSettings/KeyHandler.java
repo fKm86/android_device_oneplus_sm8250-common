10ea6f0015e8ca2d4266c7d5603a9fecd4f172fc
fKm <fmetzner86@gmail.com>
2020-08-08 12:13:31 +0200

    Revert "sm8250-common: DeviceSettings: KeyHandler: Update VibrationEffect"
    This reverts commit 001cc2d22c84bde4bd8be85ea6cd5162513d7cf0.

--- DeviceSettings/src/org/aosp/device/DeviceSettings/KeyHandler.java
+++ DeviceSettings/src/org/aosp/device/DeviceSettings/KeyHandler.java
@@ -126,10 +126,10 @@ public class KeyHandler implements DeviceKeyHandler {
     }
 
     private void doHapticFeedback() {
-        if (mVibrator != null && mVibrator.hasVibrator()) {
-            mVibrator.vibrate(VibrationEffect.createOneShot(50,
-                    VibrationEffect.DEFAULT_AMPLITUDE));
+        if (mVibrator == null) {
+            return;
         }
+	mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_POP));
     }
 
     public void handleNavbarToggle(boolean enabled) {
