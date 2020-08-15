LineageOS/android_device_oneplus_sm8150-common

sm8150-common: fod: Uprev to 1.1
Change-Id: Ia0b4c24201c4e5fc03fb5638f5281d69253f7499

fod/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_msmnile"
+#define LOG_TAG "lineage.biometrics.fingerprint.inscreen@1.1-service.oneplus_msmnile"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
@@ -24,8 +24,8 @@
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-using vendor::lineage::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreen;
-using vendor::lineage::biometrics::fingerprint::inscreen::V1_0::implementation::FingerprintInscreen;
+using vendor::lineage::biometrics::fingerprint::inscreen::V1_1::IFingerprintInscreen;
+using vendor::lineage::biometrics::fingerprint::inscreen::V1_1::implementation::FingerprintInscreen;
 
 using android::OK;
 using android::status_t;

fKm86/android_device_oneplus_sm8250-common

fod/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_kona"
+#define LOG_TAG "lineage.biometrics.fingerprint.inscreen@1.1-service.oneplus_kona"
 
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
@@ -24,8 +24,8 @@
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-using vendor::lineage::biometrics::fingerprint::inscreen::V1_0::IFingerprintInscreen;
-using vendor::lineage::biometrics::fingerprint::inscreen::V1_0::implementation::FingerprintInscreen;
+using vendor::lineage::biometrics::fingerprint::inscreen::V1_1::IFingerprintInscreen;
+using vendor::lineage::biometrics::fingerprint::inscreen::V1_1::implementation::FingerprintInscreen;
 
 using android::OK;
 using android::status_t;